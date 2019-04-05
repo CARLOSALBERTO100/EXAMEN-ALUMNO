3cd5419c833c0c99f4763bceffed739c7f329602
Carlos Alberto Cruz Amezquita <49010716+CARLOSALBERTO100@users.noreply.github.com>
2019-04-05 11:22:11 -0600

    Add files via upload

---
+++ app/src/main/java/itics/carlos/examen/myapplication/saludos.java
@@ -0,0 +1,37 @@
+package itics.carlos.examen.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class saludos extends AppCompatActivity {
+
+    TextView vnombre;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        vnombre = (TextView) findViewById(R.id.txtnombre);
+
+    }
+
+    public void Mandarsaludo(View v){
+        Intent vsaludo = new Intent(this, saludo.class);
+        vsaludo.putExtra("nombre", vnombre.getText().toString());
+        startActivity(vsaludo);
+    }
+
+    public void MensajeEmerge(View v){
+        Toast.makeText(this,"Bienvenido " + vnombre.getText().toString(),Toast.LENGTH_LONG).show();
+    }
+
+    public void Loperaciones(View v){
+        Intent intent = new Intent(this, operaciones.class);
+        startActivity(intent);
+    }
+}
